7105fd3ddc1ad815939c596994fb3c358928a47d
FabioDevGomes <fabio.dev.gomes@gmail.com>
2016-11-11 19:32:14 -0200

    Projeto final

--- src/com/fabio/main/SpringDataDemo.java
+++ src/com/fabio/main/SpringDataDemo.java
@@ -1,5 +1,35 @@
 package com.fabio.main;
 
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.fabio.bean.Employee;
+import com.fabio.dao.EmployeeDao;
+
 public class SpringDataDemo {
+
+	public static void main(String[] args) {
+		try {
+			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources\\spring-configuration.xml");
+			EmployeeDao employeeDao = (EmployeeDao) applicationContext.getBean("EmployeeDaoImpl");
+
+			Employee employee = new Employee("Employee123");
+
+			employeeDao.save(employee);
+			
+			System.out.println("Objeto salvo = "+ employee.getEmployeeId());
+			
+			Employee newEmpl = employeeDao.findByPrimaryKey(employee.getEmployeeId());
+			System.out.println(newEmpl);
+			
+			((ConfigurableApplicationContext)applicationContext).close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
